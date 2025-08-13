de6ac90d6581d37b64657a85ad3aac0d6373fcbe
mayuripy <chaudharimayuri2004@gmail.com>
2025-08-13 23:28:08 +0530

    first Commit

---
+++ Strip_Payment_Integrationn/src/main/java/com/sit/StripPaymentIntegrationnApplication.java
@@ -0,0 +1,13 @@
+package com.sit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripPaymentIntegrationnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripPaymentIntegrationnApplication.class, args);
+	}
+
+}
